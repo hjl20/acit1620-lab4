6e311c552988ad1f353486668b505fcfbc1bae2c
Herman L <hluo21@my.bcit.ca>
2023-03-06 15:35:07 -0800

    Prompt user for number and check if in courseList

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,25 @@ const courseList = [
         name: "Technical Math for IT",
     },
 ]
+
+do {
+    userNum = prompt("Enter a course number: ");
+} while (isNaN(userNum) || userNum.length != 4)
+
+inList = false;
+for (course of courseList) {
+    if (course.code.includes(userNum)) {
+        console.log(`Yes I am taking the course: ${course.code} - ${course.name}`);
+        inList = true;
+    }
+}
+if (!inList) {
+    courseList.push(
+        {
+            code: userNum,
+            name: null,
+        }
+    );
+    console.log("Course successfully added.");
+}
+
