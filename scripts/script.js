300a12e4b2923ab2573226358a206043c95e99ce
Herman L <hluo21@my.bcit.ca>
2023-03-06 14:48:05 -0800

    Create script.js and initialize list

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const courseList = [
+    {
+        code: "ACIT 1620",
+        name: "Web Fundamental Technologies",
+    },
+    {
+        code: "COMM 1116",
+        name: "Business Communications 1",
+    },
+    {
+        code: "MATH 1310",
+        name: "Technical Math for IT",
+    },
+]
