10cd18115cdcac6659f4b4cfbbcb77603ae67351
Herman L <hluo21@my.bcit.ca>
2023-03-21 16:56:04 -0700

    Remove unused courseList

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,3 @@
-const courseList = [
-    {
-        code: "ACIT 1620",
-        name: "Web Fundamental Technologies",
-    },
-    {
-        code: "COMM 1116",
-        name: "Business Communications 1",
-    },
-    {
-        code: "MATH 1310",
-        name: "Technical Math for IT",
-    },
-]
 
 const createCourseArray = () => {
     const courseList = []
