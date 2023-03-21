9398ef61cf7b435d45c03c50e2540d4387f5b2ed
Herman L <hluo21@my.bcit.ca>
2023-03-21 11:04:44 -0700

    Make createCourseArray function

--- scripts/script.js
+++ scripts/script.js
@@ -13,24 +13,53 @@ const courseList = [
     },
 ]
 
-do {
-    userNum = prompt("Enter a course number: ");
-} while (isNaN(userNum) || userNum.length != 4)
-
-inList = false;
-for (course of courseList) {
-    if (course.code.includes(userNum)) {
-        console.log(`Yes I am taking the course: ${course.code} - ${course.name}`);
-        inList = true;
+const createCourseArray = () => {
+    const courseList = []
+    // TODO: get li class=gridcourse elements to make array
+    courseNames = document.getElementsByClassName("course-name")
+    courseDates = document.getElementsByClassName("course-date")
+    for (let i = 0; i < courseNames.length; i++) {
+        courseList.push(
+            {
+                code: courseNames[i].textContent.trim(),
+                date: courseDates[i].textContent
+            }
+        )
     }
-}
-if (!inList) {
-    courseList.push(
-        {
-            code: userNum,
-            name: null,
+
+    return courseList
+} 
+
+const findCourse = courseList => {
+
+    do {
+        userNum = prompt("Enter a course number: ");
+    } while (isNaN(userNum) || userNum.length != 4)
+    
+    inList = false;
+    for (course of courseList) {
+        if (course.code.includes(userNum)) {
+            console.log(`Yes I am taking the course: ${course.code} - ${course.name}`);
+            //TODO: get element and add green-bgcolor class
+            // Get the     
+            //courseItems = document.getElementsByClassName("grid-course")
+
+            inList = true;
         }
-    );
-    console.log("Course successfully added.");
+    }
+
+    if (!inList) {
+        courseList.push(
+            {
+                code: userNum,
+                name: null,
+            }
+        );
+        console.log("Course successfully added.");
+    }   
 }
+
+courseArray = createCourseArray()
+// findCourse(courseArray)
+// findCourse(courseList)
 
